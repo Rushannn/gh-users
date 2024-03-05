c7157de243f911d16914730a7f9249dea86618ab
Rushannn <rushankarimov@gmail.com>
2024-03-05 15:53:52 +0300

    feat: add profile search rout

--- apps/gh-users-app/src/app/app.config.ts
+++ apps/gh-users-app/src/app/app.config.ts
@@ -1,9 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), provideStore(), provideEffects()],
+  providers: [
+    provideRouter([
+      {
+        path: '',
+        redirectTo: 'search',
+        pathMatch: 'full',
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('profile-search').then((m) => m.ProfileSearchComponent),
+      },
+    ]),
+    provideStore(), provideEffects()],
 };
