d90a8430dba2a7eb3a46988dbab95cc52abe814f
Rushannn <rushankarimov@gmail.com>
2024-03-05 17:26:38 +0300

    feat: add profile model

--- libs/profile/data-access/src/lib/profile.model.ts
+++ libs/profile/data-access/src/lib/profile.model.ts
@@ -1,27 +1,29 @@
+import { Profile } from '../../../../core/api-types/src/index';
+
 export type ProfileState = {
-  loggedIn: boolean;
-  user: string;
+  data: Profile
 };
 
-
-// export type GitHubUser = {
-//   login: string;
-//   id: number;
-//   node_id: string;
-//   avatar_url: string;
-//   gravatar_id: string;
-//   url: string;
-//   html_url: string;
-//   followers_url: string;
-//   following_url: string;
-//   gists_url: string;
-//   starred_url: string;
-//   subscriptions_url: string;
-//   organizations_url: string;
-//   repos_url: string;
-//   events_url: string;
-//   received_events_url: string;
-//   type: string;
-//   site_admin: boolean;
-//   score: number;
-// };
+export const profileInitialState: ProfileState = {
+  data: {
+    login: '',
+    id: 0,
+    node_id: '',
+    avatar_url: '',
+    gravatar_id: '',
+    url: '',
+    html_url: '',
+    followers_url: '',
+    following_url: '',
+    gists_url: '',
+    starred_url: '',
+    subscriptions_url: '',
+    organizations_url: '',
+    repos_url: '',
+    events_url: '',
+    received_events_url: '',
+    type: '',
+    site_admin: false,
+    score: 0
+  }
+}
--- libs/profile/data-access/src/lib/profile.store.ts
+++ libs/profile/data-access/src/lib/profile.store.ts
@@ -1,10 +1,11 @@
 import { signalStore, withMethods, withState } from '@ngrx/signals';
+import { ProfileState, profileInitialState } from './profile.model';
 
 
 export const ProfileStore = signalStore(
   { providedIn: 'root' },
   withState<ProfileState>(profileInitialState),
   withMethods(
-    ()=>({})
+    () => ({})
   )
 )
